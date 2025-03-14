0ed53051a6312905114aa6a2a5f95ddaceddace5
0xZaddyy <oyinoladapo5@gmail.com>
2025-03-14 13:10:51 +0100

    returns the price of bitcoin every 10s

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use reqwest::{self};
+use serde::Deserialize;
+use tokio::time::{sleep, Duration};
+
+#[derive(Deserialize)]
+struct ApiResponse {
+    bitcoin: CurrencyData,
+
+}
+
+#[derive(Deserialize)]
+
+struct CurrencyData {
+    usd: f64,
+}
+
+#[tokio::main]
+async fn main() {
+    let url =  "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd";
+    loop {
+        match reqwest::get(url).await {
+            Ok(response) => match response.json::<ApiResponse>().await  {
+                Ok(data) => println!("Current Bitcoin Price: ${}", data.bitcoin.usd),
+                Err(_) => println!("Failed to parse response"),
+            },
+            Err(_) => println!("Failed to fetch price"),
+        }
+        sleep(Duration::from_secs(10)).await; // Wait 10 seconds before updating
+    }
+}
